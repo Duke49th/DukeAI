a388feaf8afe64869439228d9c237f535c79f307
Duke49th <122139237+Duke49th@users.noreply.github.com>
2023-01-07 13:46:37 +0800

    update the server url

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch data from server -> bots response 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://dukeai.onrender.com/', {
     method: 'Post',
     headers: {
       'Content-Type': 'application/json'
